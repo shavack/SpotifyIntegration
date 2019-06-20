623e69c0bc3428125677e15336d347c036bb33ad
shavack <dkozuch@gmail.com>
2019-06-20 21:20:06 +0200

    Git ignore + static content wipe

--- SpotifyIntegration/Controllers/HomeController.cs
+++ SpotifyIntegration/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace SpotifyIntegration.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "About stuff";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "You will never find me";
 
             return View();
         }
